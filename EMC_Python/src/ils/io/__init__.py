dd7e8ab93255898de038c31a9385b81b4afbd145
Chuck Coughlin <ccoughlin@ils-automation.com>
2014-10-13 01:56:41 -0600

    XOM only

--- EMC_Python/src/ils/io/__init__.py
+++
@@ -1,3 +0,0 @@
-# Define the modules that are executable block classes, including BasicBlock
-# This is required for "from emc.block import *" to give the desired answer
-__all__ = ["recipe","recipedetail","basicio","opcoutput","opcconditionaloutput"]
