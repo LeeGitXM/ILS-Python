a0734bc55f2eafe9e9d41ec7cf89449c58e81bce
phassler <phassler@ils-automation.com>
2015-06-20 17:32:50 -0500

    Enhancements to better handle roles

--- ILS_Python/src/ils/common/console.py
+++ ILS_Python/src/ils/common/console.py
@@ -25,3 +25,21 @@ def startup():
         print "Opening the ", windowName
         window=system.nav.openWindow(windowName)
         system.nav.centerWindow(window)
+
+# This is called by an event handler on each console window.  The main reason for determining the role of the user is to 
+# allow the console to be closed by AE's.  (Perhaps I could allow the close button if the username != the post).
+def internalFrameOpened(event):
+    rootContainer = event.source.rootContainer
+    
+    username = system.security.getUsername()
+    rootContainer.username = username
+    
+    userRoles = system.security.getRoles()
+    if "Admin" in userRoles or "Administrator" in userRoles:
+        role = "AE"
+    elif "ae" in userRoles or "AE" in userRoles:
+        role = "AE"
+    else:
+        role = "OPERATOR"
+    
+    rootContainer.role = role
