77f7c53cb7fe1d15ebf48b184d1bacca1b33a78b
phassler <phassler@ils-automation.com>
2015-07-06 22:12:00 -0500

    Added a generic fetch as an API

--- ILS_Python/src/ils/common/associations.py
+++ ILS_Python/src/ils/common/associations.py
@@ -3,5 +3,19 @@ Created on Jul 3, 2015
 
 @author: Pete
 '''
-def hello():
-    print "Hello"
+
+import system
+    
+# Fetch all of the labBalerService Data associated with the lab data item
+def fetchSinks(source, associationType, db=''):
+
+    SQL = "Select A.sink from TkAssociation A, TkAssociationType AT "\
+        " where A.source = '%s' "\
+        " and A.AssociationTypeId = AT.AssociationTypeId "\
+        " and AT.AssocationType = '%s'" % (source, associationType)
+    
+    pds = system.db.runQuery(SQL, db)
+    sinks=[]
+    for record in pds:
+        sinks.append(record["sink"])
+    return sinks
