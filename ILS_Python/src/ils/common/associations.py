46ed508f2056c6e2bab9f58168ac2a69a76f153b
Joe <jfh249@cornell.edu>
2015-07-13 11:52:53 -0500

    associations

--- ILS_Python/src/ils/common/associations.py
+++ ILS_Python/src/ils/common/associations.py
@@ -19,3 +19,67 @@ def fetchSinks(source, associationType, db=''):
     for record in pds:
         sinks.append(record["sink"])
     return sinks
+
+#open transaction when window is opened
+def internalFrameOpened(rootContainer):
+    txID = system.db.beginTransaction(timeout=300000)
+    rootContainer.txID = txID
+    update(rootContainer)
+    
+#refresh when window is activated
+def internalFrameActivated(rootContainer):
+    update(rootContainer)
+
+#open transaction when window is opened
+def internalFrameClosing(rootContainer):
+    try:
+        txId=rootContainer.txID
+        system.db.rollbackTransaction(txId)
+        system.db.closeTransaction(txId)
+    except:
+        print "Caught an error trying to close the transaction"
+        
+#update the window
+def update(rootContainer):
+    txID = rootContainer.txID
+    table = rootContainer.getComponent("Power Table")
+    
+    #update associations table
+    SQL = "SELECT A.AssociationId, A.Source, A.Sink, A.AssociationTypeId, T.AssociationType "\
+        " FROM TkAssociation A, TkAssociationType T "\
+        " WHERE A.AssociationTypeId = T.AssociationTypeId"
+    print SQL
+    pds = system.db.runQuery(SQL, tx=txID)
+    table.data = pds
+    
+#remove the selected row
+def removeRow(event):
+    rootContainer = event.source.parent
+    txID = rootContainer.txID
+    table = rootContainer.getComponent("Power Table")
+    ds = table.data
+    
+    row = table.selectedRow
+    associationId = ds.getValueAt(row, "AssociationId")
+            
+    #remove the selected row
+    SQL = "DELETE FROM TkAssociation "\
+        " WHERE AssociationId = %i "\
+        % (associationId)
+    system.db.runUpdateQuery(SQL, tx=txID)
+    
+    update(rootContainer)
+    
+#add a row
+def insertRow(event):
+    rootContainer = event.source.parent
+    txID = rootContainer.txID
+    
+    source = rootContainer.getComponent("New Source").text
+    sink = rootContainer.getComponent("New Sink").text
+    associationTypeId = rootContainer.getComponent("Dropdown").selectedValue
+        
+    sql = "INSERT INTO TkAssociation (Source, Sink, AssociationTypeId)"\
+        "VALUES ('%s', '%s', '%s')" % (source, sink, associationTypeId)
+    print sql
+    system.db.runUpdateQuery(sql, tx=txID)
