bcde0d52f785065e30eec215acefa65ebc7dc24f
Chuck Coughlin <ccoughlin@ils-automation.com>
2015-10-13 07:46:17 -0600

    Update for testing.

--- ILS_Python/src/pylib/timeFactor.py
+++ ILS_Python/src/pylib/timeFactor.py
@@ -1,4 +1,16 @@
 # Copyright 2015. ILS Automation. All rights reserved.
+# Read the timestamp of a tag and return as an integer
+# milliseconds since the start of the Unix epoch.
+def getTagTime(provider,tpath):
+	# Convert to the proper provider
+	path = "["+provider+"]"+tpath
+	print "getTagTime: ",path
+	qv =system.tag.read(path)
+	print "getTagTime: completed read"
+	msecs = qv.getTimestamp().getTime()
+	print "getTagTime: ",msecs
+	return msecs
+
 # Argument is the diagram path
 def setTestTimeOffset(common,yr,mon,dy,hr,min,sec):
 	import datetime,time
