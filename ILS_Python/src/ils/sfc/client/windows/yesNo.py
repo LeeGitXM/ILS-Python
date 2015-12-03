40b26bdccd4bdf1fa6f069f59739415e6b5ae7b7
phassler <phassler@ils-automation.com>
2015-12-03 17:56:05 -0600

    New module to support Yes/No as a Vision window

--- ILS_Python/src/ils/sfc/client/windows/yesNo.py
+++ ILS_Python/src/ils/sfc/client/windows/yesNo.py
@@ -4,11 +4,27 @@ Created on Dec 3, 2015
 @author: rforbes
 '''
 
-def populate(window):
-    promptField = window.getRootContainer().getComponent('prompt')
-    promptField.setText(window.getRootContainer().prompt)
-    
-def sendResponse(window, response):   
-    from ils.sfc.common.constants import RESPONSE
-    from ils.sfc.client.windowUtil import responseWindowClosed
-    responseWindowClosed(window, response)
+import system
+
+def yesActionPerformed(event):
+    rootContainer=event.source.parent
+    messageId=rootContainer.messageId
+    sendResponse(messageId, "Yes")
+    system.nav.closeParentWindow(event)
+  
+def noActionPerformed(event):
+    rootContainer=event.source.parent
+    messageId=rootContainer.messageId
+    sendResponse(messageId, "No")
+    system.nav.closeParentWindow(event)
+
+def timeoutActionPerformed(event):
+    rootContainer=event.source.parent
+    messageId=rootContainer.messageId
+    sendResponse(messageId, "Timeout")
+    system.nav.closeParentWindow(event)
+
+def sendResponse(messageId, textResponse): 
+    # I'm not sure who this message is going to - presumably the gateway handler
+    from ils.sfc.client.util import sendResponse 
+    sendResponse(messageId, textResponse)
