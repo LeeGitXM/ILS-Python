d4d107bb5997c80b385bd3ec085586879cb1a36b
phassler <phassler@ils-automation.com>
2016-01-20 15:21:19 -0600

    Added some logic that attempts to reset the recipe data but it isn't on the control tread (the code here may be correct but the guy calling it may need to call it on the control thread the first time.)

--- ILS_Python/src/ils/sfc/gateway/steps/yesNo.py
+++ ILS_Python/src/ils/sfc/gateway/steps/yesNo.py
@@ -13,7 +13,21 @@ def activate(scopeContext, stepProperties, deactivate):
     from ils.sfc.common.util import isEmpty
     from ils.sfc.gateway.steps import commonInput
     from ils.sfc.gateway.util import getStepProperty
-    from system.ils.sfc.common.Constants import BUTTON_LABEL
+    from ils.sfc.gateway.api import s88Set
+    from system.ils.sfc.common.Constants import BUTTON_LABEL, RECIPE_LOCATION, KEY
+    from ils.sfc.common.constants import WAITING_FOR_REPLY
+    print "In yesNo.activate()..."
+    
+    stepScope = scopeContext.getStepScope()
+    chartScope = scopeContext.getChartScope()
+    
+    waitingForReply = stepScope.get(WAITING_FOR_REPLY, False);    
+    if not waitingForReply:
+        print "*** Initialize the recipe data ****"
+        recipeLocation = getStepProperty(stepProperties, RECIPE_LOCATION) 
+        key = getStepProperty(stepProperties, KEY) 
+        s88Set(chartScope, stepScope, key, "??????", recipeLocation)
+        
     buttonLabel = getStepProperty(stepProperties, BUTTON_LABEL)
     if isEmpty(buttonLabel):
         buttonLabel = 'Y/N'
