0cedffdb4f932c0088a1934beba0a8cc88b7b0a6
phassler <phassler@ils-automation.com>
2016-01-21 11:31:25 -0600

    First pass at redesign for new window framework.  Window opens and registers with the control panel.

--- ILS_Python/src/ils/sfc/gateway/steps/monitorDownload.py
+++ ILS_Python/src/ils/sfc/gateway/steps/monitorDownload.py
@@ -5,13 +5,13 @@ Created on Dec 17, 2015
 '''
 
 def activate(scopeContext, stepProperties, deactivate): 
-    from system.ils.sfc.common.Constants import RECIPE_LOCATION, MONITOR_DOWNLOADS_CONFIG, DATA_ID, DOWNLOAD_STATUS, WRITE_CONFIRMED
-    from ils.sfc.common.constants import PV_VALUE, PV_MONITOR_ACTIVE, PV_MONITOR_STATUS, STEP_PENDING,  PV_NOT_MONITORED
+    from system.ils.sfc.common.Constants import BUTTON_LABEL, RECIPE_LOCATION, MONITOR_DOWNLOADS_CONFIG, DATA_ID, DOWNLOAD_STATUS, WRITE_CONFIRMED, POSITION, SCALE, WINDOW_TITLE, PROMPT, KEY
+    from ils.sfc.common.constants import PV_VALUE, PV_MONITOR_ACTIVE, PV_MONITOR_STATUS, STEP_PENDING,  PV_NOT_MONITORED, WINDOW_ID
     from system.ils.sfc import getMonitorDownloadsConfig
     from ils.sfc.gateway.downloads import handleTimer
     from ils.sfc.gateway.monitoring import createMonitoringMgr
-    from ils.sfc.gateway.util import sendMessageToClient, getStepProperty, transferStepPropertiesToMessage, handleUnexpectedGatewayError
-    from system.ils.sfc import getProviderName
+    from ils.sfc.gateway.util import sendMessageToClient, getStepProperty, transferStepPropertiesToMessage, handleUnexpectedGatewayError, getControlPanelId, createWindowRecord
+    from system.ils.sfc import getProviderName, getDatabaseName
     from ils.sfc.gateway.api import getIsolationMode, getChartLogger, getProject
     from ils.sfc.gateway.recipe import RecipeData
     
@@ -19,13 +19,14 @@ def activate(scopeContext, stepProperties, deactivate):
         chartScope = scopeContext.getChartScope()
         stepScope = scopeContext.getStepScope()
         logger = getChartLogger(chartScope)
+        logger.trace("In monitorDownload.activate()...")
         timer, timerAttribute = handleTimer(chartScope, stepScope, stepProperties, logger)
         recipeLocation = getStepProperty(stepProperties, RECIPE_LOCATION)
         configJson = getStepProperty(stepProperties, MONITOR_DOWNLOADS_CONFIG)
         monitorDownloadsConfig = getMonitorDownloadsConfig(configJson)
         isolationMode = getIsolationMode(chartScope) 
         providerName = getProviderName(isolationMode)
-        logger.info("Starting a download monitor...")
+        database = getDatabaseName(isolationMode)
     
         # Reset the recipe data download and PV monitoring attributes
         for row in monitorDownloadsConfig.rows:
@@ -40,11 +41,25 @@ def activate(scopeContext, stepProperties, deactivate):
             rd.set(PV_MONITOR_STATUS, PV_NOT_MONITORED)
     
         mgr = createMonitoringMgr(chartScope, stepScope, recipeLocation, timer, timerAttribute, monitorDownloadsConfig, logger, providerName)
+        
+        # Insert a window record into the database
+        controlPanelId = getControlPanelId(chartScope)
+        buttonLabel = getStepProperty(stepProperties, BUTTON_LABEL)
+        position = getStepProperty(stepProperties, POSITION)
+        scale = getStepProperty(stepProperties, SCALE)
+        title = getStepProperty(stepProperties, WINDOW_TITLE)
+        windowType = "SFC/MonitorDownloads"
+        windowId = createWindowRecord(controlPanelId, windowType, buttonLabel, position, scale, title, database)
+        stepScope[WINDOW_ID] = windowId     # This step completes as soon as the GUI is posted do I doubt I need to save this.
+        
         payload = dict()
         payload[DATA_ID] = mgr.getTimerId()
+        payload[WINDOW_ID] = windowId
+        
         transferStepPropertiesToMessage(stepProperties, payload)
         project = getProject(chartScope)
-        sendMessageToClient(project, 'sfcMonitorDownloads', payload)             
+        sendMessageToClient(project, 'sfcOpenWindow', payload)
+        logger.trace("...leaving monitorDownload.activate()")      
     except:
         handleUnexpectedGatewayError(chartScope, 'Unexpected error in monitorDownload.py', logger)
     finally:
