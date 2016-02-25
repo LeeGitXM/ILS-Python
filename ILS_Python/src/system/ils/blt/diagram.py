a0f7e11c7b3eeb249dd34d13cc3c320c1b1d199f
phassler <phassler@ils-automation.com>
2016-02-25 09:05:03 -0600

    Updated for new APIs

--- ILS_Python/src/system/ils/blt/diagram.py
+++ ILS_Python/src/system/ils/blt/diagram.py
@@ -3,41 +3,46 @@
 
 @author: chuckc
 '''
-def getDiagramDescriptors(diagid):
-    return []
-
-def getDiagramState(diagid):
-    return "ACTIVE"
-
-def getHandler():
-    '''Return a python request handler'''
-    
-def getToolkitProperty(name):
-    return name
-   
-def resetDiagram(diagid):
-    '''Reset the specified diagram'''
-
-def resetBlock(diagid, blockid):
-    '''Reset the specified block'''
-    
-def setDiagramState(diagid,state):
-    '''Set the state of the specified diagram'''
-    
-def getDiagramForBlock(blockId):
-    '''Get the diagram serializable block state descriptor'''
 
 def getBlockState(diagramId, blockName):
     '''Get the internal state of a block'''
+
+def getDiagramDescriptors(diagid):
+    return []
+
+def getDiagramForBlock(blockId):
+    '''Get the diagram serializable block state descriptor'''
     
+def getDiagramState(diagid):
+    return "ACTIVE"
+    
+def getHandler():
+    '''Return a python request handler'''
+
 # I don't think this is correct...
 def getPropertyValue(diagramId, blockId, property):
     '''Get the diagram serializable block state descriptor'''
     
-def getRequestHandlere():
+def getRequestHandler():
     '''Get the controller request handler'''
+    
+def getToolkitProperty(name):
+    return name
 
 def listBlocksUpstreamOf(diagramId, sqcBlockName):
     '''Get a list of serializable block state descriptor all of the upstream blocks'''
-def resetBlock(diagramId,blockId):
-    '''Reset a block within a diagram'''
+
+def resetBlock(diagId, blockName):
+    '''Reset the specified block'''
+     
+def resetDiagram(diagid):
+    '''Reset the specified diagram'''
+
+def sendSignal(diagramUUID, blockName, signal, message):
+    '''Send a signal to the named block on the specified diagram'''
+    
+def setDiagramState(diagid,state):
+    '''Set the state of the specified diagram'''
+
+def setState(diagId, blockName, state):
+    '''Set the state of the named block on the specified diagram'''
