9434486fc3f04e97703c6e5df80de7fbeb641b92
U-rforbes-laptop\rforbes <rforbes@rforbes-laptop.(none)>
2016-05-13 12:42:41 -0600

    fixes for Trac 420-422

--- ILS_Python/src/ils/sfc/gateway/steps/enableDisable.py
+++ ILS_Python/src/ils/sfc/gateway/steps/enableDisable.py
@@ -7,7 +7,7 @@ Created on Dec 17, 2015
 def activate(scopeContext, stepProperties, deactivate):
     from  system.ils.sfc.common.Constants import ENABLE_PAUSE, ENABLE_RESUME, ENABLE_CANCEL
     from ils.sfc.gateway.util import getStepProperty, getControlPanelId, handleUnexpectedGatewayError
-    from ils.sfc.gateway.api import getChartLogger
+    from ils.sfc.gateway.api import getChartLogger, getDatabaseName
     import system.db
     
     try:
@@ -17,7 +17,8 @@ def activate(scopeContext, stepProperties, deactivate):
         enableResume = getStepProperty(stepProperties, ENABLE_RESUME)
         enableCancel = getStepProperty(stepProperties, ENABLE_CANCEL)
         controlPanelId = getControlPanelId(chartScope)
-        system.db.runUpdateQuery("update SfcControlPanel set enablePause = %d,  enableResume = %d,  enableCancel = %d where controlPanelId = '%s'" % (enablePause, enableResume, enableCancel, controlPanelId))
+        database = getDatabaseName(chartScope)
+        system.db.runUpdateQuery("update SfcControlPanel set enablePause = %d,  enableResume = %d,  enableCancel = %d where controlPanelId = %s" % (enablePause, enableResume, enableCancel, controlPanelId), database)
     except:
         handleUnexpectedGatewayError(chartScope, 'Unexpected error in enableDisable.py', chartLogger)
     finally:
