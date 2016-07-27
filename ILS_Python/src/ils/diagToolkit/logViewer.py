d50f3e6d19e31c145072e42f6298ca9fd591e880
phassler <phassler@ils-automation.com>
2016-07-27 15:18:34 -0500

    Python to support new Final Diagnosis Log Viewer

---
+++ ILS_Python/src/ils/diagToolkit/logViewer.py
@@ -0,0 +1,31 @@
+'''
+Created on Jul 26, 2016
+
+@author: ils
+'''
+
+import system
+
+def update(rootContainer):
+    print "Updating..."
+    
+    table = rootContainer.getComponent('Power Table')
+    
+    startDate = rootContainer.getComponent('Date Range').startDate
+    endDate = rootContainer.getComponent('Date Range').endDate
+
+    SQL = "select LogId, Timestamp, ApplicationName, FamilyName, FamilyPriority, FinalDiagnosisName, FinalDiagnosisPriority, State, Active "\
+        " from DtFinalDiagnosisLogView " \
+        " where Timestamp > ? and Timestamp < ?" \
+        " order by LogId"
+
+
+#    SQL = "select convert(varchar(50), Timestamp, 9), ApplicationName, FamilyName, FamilyPriority, FinalDiagnosisName, FinalDiagnosisPriority, Active "\
+#        " from DtFinalDiagnosisLogView " \
+#        " where Timestamp > ? and Timestamp < ?" \
+#        " order by Timestamp DESC"
+
+    print SQL
+    
+    pds = system.db.runPrepQuery(SQL, [startDate, endDate])
+    table.data = pds
