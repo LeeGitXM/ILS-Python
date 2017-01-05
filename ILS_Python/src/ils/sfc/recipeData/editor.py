f5b5e045902288c115d84dd490fe17cfa499f9b6
Pete-asus <phassler@ils-automation.com>
2017-01-05 09:51:09 -0600

    Work on SFC Recipe data V2

--- ILS_Python/src/ils/sfc/recipeData/editor.py
+++ ILS_Python/src/ils/sfc/recipeData/editor.py
@@ -5,7 +5,7 @@ Currently, when editing recipe data in the Designer, we are alway editing the Pr
 @author: phass
 '''
 
-import system
+import system, string
 from ils.common.database import toDictList
 from ils.common.config import getDatabaseClient 
 
@@ -16,11 +16,30 @@ def getRecipeDataList(stepUUID):
     recipeData = []
     
     pds = system.db.runQuery("select * from SfcRecipeDataSimpleValueView where stepUUID = '%s'" % (stepUUID), db)
-    recipeData = toDictList(pds, recipeData)
+    if len(pds) > 0:
+        print "Fetched %i simple value rows..." % (len(pds))
+        pds = convertValues(pds)
+        recipeData = toDictList(pds, recipeData)
     
     pds = system.db.runQuery("select * from SfcRecipeDataOutputView where stepUUID = '%s'" % (stepUUID), db)
-    recipeData = toDictList(pds, recipeData)
+    if len(pds) > 0:
+        print "Fetched %i output rows..." % (len(pds))
+        recipeData = toDictList(pds, recipeData)
     
     print "Fetched recipe data: ", recipeData
     
     return recipeData
+
+
+# Consolidate the 4 value columns into a single one 
+def convertValues(pds):
+    
+    ds = system.dataset.toDataSet(pds)
+    vals = []
+    for i in range(0,ds.rowCount):
+        valueType = ds.getValueAt(i,"ValueType")
+        val = ds.getValueAt(i,"%sVALUE" % string.upper(valueType))
+        vals.append(str(val))
+    ds = system.dataset.addColumn(ds, vals, "Value", basestring)
+    pds = system.dataset.toPyDataSet(ds)
+    return pds
