33719b4cb22df5085773aca66d85f079702afad1
Pete-asus <phassler@ils-automation.com>
2017-05-25 23:44:00 -0500

    Modified to set the Queue attribute for the control parameter record.

--- ILS_Python/src/ils/sfc/gateway/steps/operation.py
+++ ILS_Python/src/ils/sfc/gateway/steps/operation.py
@@ -5,7 +5,7 @@ Created on Dec 16, 2015
 '''
 from ils.sfc.gateway.util import getStepProperty, getTopChartRunId, handleUnexpectedGatewayError
 from ils.sfc.gateway.api import getDatabaseName, getChartLogger
-from ils.sfc.common.constants import NAME
+from ils.sfc.common.constants import NAME, MESSAGE_QUEUE
 import system
     
 def activate(scopeContext, stepProperties, state):
@@ -13,11 +13,14 @@ def activate(scopeContext, stepProperties, state):
     try:
         chartScope = scopeContext.getChartScope()
         stepName = getStepProperty(stepProperties, NAME)
-        chartLogger = getChartLogger(chartScope)
-        database = getDatabaseName(chartScope)
+        messageQueue = getStepProperty(stepProperties, MESSAGE_QUEUE)
+        logger = getChartLogger(chartScope)
+        db = getDatabaseName(chartScope)
         chartRunId = getTopChartRunId(chartScope)
-        system.db.runUpdateQuery("update SfcControlPanel set operation = '%s' where chartRunId = '%s'" % (stepName, chartRunId), database)
+        rows = system.db.runUpdateQuery("update SfcControlPanel set operation = '%s', MsgQueue = '%s' where chartRunId = '%s'" % (stepName, messageQueue, chartRunId), database=db)
+        if rows <> 1:
+            logger.errorf("Error updating the SfcControlPanel info for operation <%s> and queue <%s> using chartRunId: %s", stepName, messageQueue, chartRunId)
     except:
-        handleUnexpectedGatewayError(chartScope, stepProperties, 'Unexpected error in operation.py', chartLogger)
+        handleUnexpectedGatewayError(chartScope, stepProperties, 'Unexpected error in operation.py', logger)
     finally:
         return True
