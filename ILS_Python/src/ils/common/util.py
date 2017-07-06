d18fd58416f121888785002867e90b99d3c755d4
Pete-asus <phassler@ils-automation.com>
2017-07-05 21:10:55 -0500

    Moved some really generic wrappers for querying tag history.

--- ILS_Python/src/ils/common/util.py
+++ ILS_Python/src/ils/common/util.py
@@ -122,3 +122,74 @@ A little utility for a one liner that I can never remember.  Clear a dataset kee
 def clearDataset(ds):
     ds = system.dataset.deleteRows(ds, range(ds.rowCount))
     return ds
+
+'''
+return a dataset with one row.  The first column is a timestamp and each subsequent column is a tags aggregated value, one value for each tag.
+It also returns a flag that indicates if any one of the tags is Nan, or None.  This query does not return a quality.
+'''
+def readAverageValues(tagPaths, tagProvider, timeIntervalMinutes, log):
+    fullTagPaths = []
+    for tagPath in tagPaths:
+        fullTagPaths.append("[%s]%s" % (tagProvider, tagPath))
+    
+    ds = system.tag.queryTagHistory(
+        paths=fullTagPaths, 
+        endDate=system.date.now(), 
+        rangeMinutes=-1*timeIntervalMinutes, 
+        aggregationMode="Average", 
+        returnSize=1, 
+        ignoreBadQuality=True
+        )
+    
+    badValue = False
+    for i in range(0,len(tagPaths)):
+        isGood = ds.getQualityAt(0, i + 1).isGood()
+        print "Average value for %s isGood %s" % (tagPaths[i], str(isGood))
+        if not(isGood):
+            badValue = True
+            log.warnf("Unable to collect average value for %s", tagPaths[i])
+
+    return badValue, ds
+
+'''
+Return a list of qualified values and a flag that indicates if any one of the tags is bad or None
+'''
+def readInstantaneousValues(tagPaths, tagProvider, log):
+    fullTagPaths = []
+    for tagPath in tagPaths:
+        fullTagPaths.append("[%s]%s" % (tagProvider, tagPath))
+    
+    qvs = system.tag.readAll(fullTagPaths)
+    
+    badValue = False
+    for i in range(0,len(tagPaths)):
+        qv = qvs[i]
+        print "Instantaneous value for %s is %s" % (tagPaths[i], qv)
+        if not(qv.quality.isGood()):
+            badValue = True
+            log.warnf("Read a bad value for %s - %s", tagPaths[i], str(qv))
+            
+    return badValue, qvs
+
+'''
+This is a test of querying history to determine if we should use the tag provider name or the history tag provider name
+(This should prove that we should use the tagProvider).
+'''
+def test():
+    log = system.util.getLogger("Test")
+    tagPaths=[]
+#    tagPaths.append("SFC IO/Rate Change/VRF9101Z/value")  #POLY-RATE
+    tagPaths.append("SFC IO/Rate Change/VRF402Z-1/value") #MAIN-C2-RATE
+    tagPaths.append("SFC IO/Rate Change/VRF403R-3/value") #MAIN-C3-TO-C2-RATIO
+    tagPaths.append("SFC IO/Rate Change/VCF000R-3/value") #VA-TO-C2-RATIO
+    tagPaths.append("SFC IO/Rate Change/VRG401Z-1/value") #CEMENT-CONCENTRATION
+    tagPaths.append("SFC IO/Cold Stick General/VRT701S-1/value") #RX-INLET-TEMP-PV
+    tagPaths.append("SFC IO/Cold Stick General/VRT700S-3/value") #OUTLET-TEMP-PV
+    tagPaths.append("SFC IO/Cold Stick General/VCF262R-2/value") #AL-TO-VA
+
+    badValue, ds = readAverageValues(tagPaths, "XOMhistory", 30, log)
+    print "Reading historic average, isBad = ", badValue
+    
+    badValue, qvs = readInstantaneousValues(tagPaths, "XOM", log)
+    print "Reading current values, isBad = ", badValue
+    
