082e5a03aa099e2af7fc819d2ec65cca8e2d2972
Pete-asus <phassler@ils-automation.com>
2019-07-04 23:57:40 -0500

    New file, e-mailed to Marshall

---
+++ ILS_Python/src/ils/uir/common.py
@@ -0,0 +1,24 @@
+'''
+Created on Jul 1, 2019
+
+@author: phass
+'''
+
+import system
+
+def findFromAddress(post):
+    '''
+    Get the from address and the pile prefix for the specified post from the email configuration dataset tag
+    '''
+    ds = system.tag.read("[XOM]Configuration/UIR/fromList").value
+
+    fromEmail = "UIR"
+    filePrefix = ""
+    
+    for row in range(ds.rowCount):
+        postContains = ds.getValueAt(row,"Post Contains")
+        if post.find(postContains) != -1:
+            fromEmail = ds.getValueAt(row,"From Email")
+            filePrefix = ds.getValueAt(row,"File Prefix")
+            
+    return fromEmail, filePrefix
