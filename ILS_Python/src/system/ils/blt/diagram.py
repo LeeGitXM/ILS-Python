323246118e44870ca9b6c6c74f67f8bcd80d434c
Craig Lovold <clovold@ils-automation.com>
2019-11-25 09:46:29 -0600

    updated function list exposed to python

--- ILS_Python/src/system/ils/blt/diagram.py
+++ ILS_Python/src/system/ils/blt/diagram.py
@@ -10,8 +10,24 @@ def clearWatermark(diagId):
 def getBlockId(diagramId, blockName):
     '''Get the ID of a block'''
 
+def getApplicationName(appId):
+    '''Get the name of the application by its UUID'''
+
 def getBlockState(diagramId, blockName):
     '''Get the internal state of a block'''
+
+def getControllerState(appId):
+    '''Get the current state of the controller'''
+
+def getDatabaseForUUID(uuid):
+    '''Get the database associated with a UUID'''
+
+def getDatasourceNames():
+    '''Get available database source names'''
+
+def getDiagram(diagramIdid):
+    '''Get the descriptor for the diagram that corresponds to the supplied Id'''
+
 def getProductionDatabase():
     return "DATABASE"
 
@@ -36,9 +52,18 @@ def getDiagramState(diagid):
 def getExplanation(diagid,blockId):
     return "because."
 
+def getFamilyName(uuid):
+    '''Get the family name that corresponds to the supplied Id'''
+
 def getHandler():
     '''Return a python request handler'''
 
+def getInternalState(diagid, blockId):
+    ''' return the the internal state of a block '''
+
+def getPropertyBinding(diagId, blockId, prop):
+    '''Get the binding (tag path) of a specified block property'''
+    
 # I don't think this is correct...
 def getPropertyValue(diagId, blockId, prop):
     '''Get the diagram serializable block state descriptor'''
@@ -46,6 +71,9 @@ def getPropertyValue(diagId, blockId, prop):
 def getRequestHandler():
     '''Get the controller request handler'''
     
+def getTimeOfLastBlock(diagId, blockName):
+    '''  get the time at which the block last changed its state '''
+
 def getToolkitProperty(name):
     return name
 
@@ -103,6 +131,12 @@ def sendSignal(diagId, blockName, signal, message):
 def sendTimestampedSignal(diagid,command,message,arg,ts):
     '''Send a signal to the named block on the specified diagram'''
 
+def setApplicationState(appName, state):
+    '''Change the state of every diagram in the named application to the specified state.'''
+    
+def setBlockProperty(diagId, blockName, propertyName, value):
+    ''' Change the value of a block property in such a way that the block and UI are notified of the change.'''
+
 def setBlockState(diagId, blockName, blockState):
     '''Set the internal state of a block, states are TRUE, FALSE, UNKNOWN, UNSET'''
     
@@ -111,6 +145,12 @@ def setDiagramState(diagId,state):
 
 def setState(diagId, blockName, state):
     '''Set the state of the named block on the specified diagram'''
+    
+def setTimeFactor(timeFactor):
+    '''Set the time factor'''
+    
+def setToolkitProperty(propertyName, value):
+    ''' Save a value into the HSQL database table associated with the toolkit. The table contains name-value pairs, so any name is allowable.'''
     
 def setWatermark(diagId, txt):
     '''Add a watermark with desired text to the diagram'''
