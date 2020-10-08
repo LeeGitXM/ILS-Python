bae3b19209e17e30259950eed6c916b0b26fe738
Pete-asus <phassler@ils-automation.com>
2020-10-08 14:58:47 -0500

    Added another arg to specify the dataset index.  Also added a logger.

--- ILS_Python/src/ils/jChart/xyColor.py
+++ ILS_Python/src/ils/jChart/xyColor.py
@@ -4,21 +4,23 @@ for a jfreechart xy scatter plot
 
 @author: phass
 '''
+import system
 import org.jfree.chart.renderer.xy.AbstractXYItemRenderer as AbstractXYItemRenderer
 import java.awt.Color as Color
 import java.awt.geom.Rectangle2D as Rectangle2D
 import org.jfree.chart.plot.PlotOrientation as PlotOrientation
 import org.jfree.util.ShapeUtilities as ShapeUtilities
 from ils.jChart.common import getPlot
+log = system.util.getLogger("com.ils.jChart.xyColor")
 
 # Configure the chart with a custom renderer that colors
 # the points based on their place within the sequence.
 # c1 and c2 are the starting and ending colors.
-def configure(chart, jChart, c1, c2):
-    print "In %s.configure: (%d,%d,%d)->(%d,%d,%d)" % (__name__,c1.getRed(),c1.getGreen(),c1.getBlue(),c2.getRed(),c2.getGreen(),c2.getBlue())
+def configure(chart, jChart, c1, c2, datasetIndex):
+    log.infof("In %s.configure: dataset index: %d, color: (%d,%d,%d)->(%d,%d,%d)", __name__, datasetIndex, c1.getRed(), c1.getGreen(), c1.getBlue(), c2.getRed(), c2.getGreen(), c2.getBlue())
     renderer = colorRenderer(c1,c2)
     plot = getPlot(jChart,0)
-    plot.setRenderer(0,renderer)
+    plot.setRenderer(datasetIndex,renderer)
 
  
 # Render scatterplot points with different colors for each
@@ -26,9 +28,12 @@ def configure(chart, jChart, c1, c2):
 class colorRenderer (AbstractXYItemRenderer):
      
     def __init__(self,c1,c2):
+        log.tracef("Initializing a new renderer!")
         self.c1 = c1
         self.c2 = c2
-        #print "Initializing a new renderer!"
+        log.tracef("start color: (%d, %d, %d, %d)", c1.getRed(), c1.getGreen(), c1.getBlue(), c1.getAlpha())
+        log.tracef("end color: (%d, %d, %d, %d)", c2.getRed(), c2.getGreen(), c2.getBlue(), c2.getAlpha())
+        
         
     # Overrite
     def drawItem(self,g2, state, dataArea, info, plot, domainAxis, rangeAxis, dataset,  series, item, crosshairState, success):
@@ -45,10 +50,13 @@ class colorRenderer (AbstractXYItemRenderer):
         elif orientation == PlotOrientation.VERTICAL:
             shape = ShapeUtilities.createTranslatedShape(shape, transX,transY)
         
-        count = plot.getDataset().getItemCount(series) - 1
+#        count = plot.getDataset().getItemCount(series) - 1
+        count = dataset.getItemCount(series) - 1
         if count<1: count = 1
+        log.tracef("drawItem: (%f, %f) - %d of %d", x, y, int(item),int(count))
+        
         color = self.interpolate(self.c1,self.c2, float(item)/count)
-        #print "drawItem %d of %d: (%d,%d,%d)" % (int(item),int(count),int(color.getRed()),int(color.getGreen()),color.getBlue())
+        log.tracef("   using interpolated color: (%d,%d,%d)", int(color.getRed()), int(color.getGreen()), int(color.getBlue()))
         g2.setPaint(color)
         g2.fill(shape)
         g2.draw(shape)
@@ -56,12 +64,10 @@ class colorRenderer (AbstractXYItemRenderer):
 
     # frac is (0.,1.]
     def interpolate(self,c1,c2,frac):
-        #print "drawItem: c1= ",c1.getRed(),c1.getGreen(),c1.getBlue(),c1.getAlpha()
         red   = c1.getRed()*frac   + c2.getRed()*(1.-frac)
         green = c1.getGreen()*frac + c2.getGreen()*(1.-frac)
         blue  = c1.getBlue()*frac  + c2.getBlue()*(1.-frac)
         alpha = c1.getAlpha()*frac + c2.getAlpha()*(1.-frac)
-
-        #print "drawItem: ",red,green,blue,alpha
+        
         # Args are float values in the range (0,1)
         return Color(red/255., green/255., blue/255., alpha/255.)      
