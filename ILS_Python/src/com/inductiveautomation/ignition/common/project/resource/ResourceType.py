5b0b2d5d958e0c034ee1be607b4fbaf1f4b2e357
phassler <phassler@ils-automation.com>
2022-08-09 17:54:28 -0500

    Try to make eclipse happy

---
+++ ILS_Python/src/com/inductiveautomation/ignition/common/project/resource/ResourceType.py
@@ -0,0 +1,17 @@
+'''
+Created on Aug 9, 2022
+
+@author: ils
+'''
+
+class ResourceType(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
