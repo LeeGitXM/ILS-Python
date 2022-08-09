1a8c9444420a2882b8b53c624780aa9c22d6ca71
phassler <phassler@ils-automation.com>
2022-08-09 17:22:10 -0500

    Additions to make eclipse happy

---
+++ ILS_Python/src/com/ils/blt/common.py
@@ -0,0 +1,8 @@
+'''
+Created on Aug 9, 2022
+
+@author: ils
+'''
+
+def ApplicationRequestHandler():
+    print "Yo!"
---
+++ ILS_Python/src/com/ils/blt/gateway.py
@@ -0,0 +1,8 @@
+'''
+Created on Aug 9, 2022
+
+@author: ils
+'''
+
+def ControllerRequestHandler():
+    print "Yo"
